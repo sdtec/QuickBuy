bc5f1775ad0d459d286088b956c89e9350e669c0
Sidney Souza <sidney@sdtec.com.br>
2019-07-09 15:03:16 -0400

    Criação das Entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,38 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public Boolean Ehboleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public Boolean EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+
+        }
+
+        public Boolean EhCartaoDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+
+        }
+
+        public Boolean ÑaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+
+        }
+    }
+
+}
