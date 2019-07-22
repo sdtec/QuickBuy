cc8995d7b575f36ca1c837f17fd8f7c6b5c043b3
Sidney Souza <sidney@sdtec.com.br>
2019-07-22 18:33:36 -0400

    evolução

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,8 +1,15 @@
+import {Component} from "@angular/core"
+@Component({
+  selector: "app-produto",
+  template: "<html><body>{{obterNome()}}</body></html>"
+})
+
 export class ProdutoComponent {
   public nome: string;
   public liberadoParaVenda: boolean;
 
   public obterNome(): string {
+    this.nome = "SAMSUNG";
     return this.nome;
   }
 
